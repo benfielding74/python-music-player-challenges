5136125c611cd71c03d2728190551aad0b29917b
benfielding74 <billy.fadden@live.com>
2022-02-23 18:04:34 +0000

    created tests that drove creation of methods to add, remove and list tracks in the MusicLibrary class

--- seed/player/music_library.py
+++ seed/player/music_library.py
@@ -1,2 +1,19 @@
 class MusicLibrary:
-    pass
+
+    def __init__(self):
+        self.trackList = []
+
+    def add(self, track):
+        self.track = track
+        self.trackList.append(self.track)
+
+    def remove(self, track):
+        self.track = track
+        if self.track < len(self.trackList):
+            del self.trackList[self.track]
+            return True
+        else:
+            return False
+
+    def all(self):
+        return self.trackList
--- seed/tests/test_music_library.py
+++ seed/tests/test_music_library.py
@@ -6,3 +6,25 @@ from player.music_library import MusicLibrary
 class TestMusicLibrary(unittest.TestCase):
     def test_constructs(self):
         MusicLibrary()
+
+    def test_adds_item_to_library(self):
+        music_library = MusicLibrary()
+        music_library.add("Rolling Blackouts by The Go! Team")
+        self.assertEqual(music_library.trackList, ["Rolling Blackouts by The Go! Team"])
+
+    def test_lists_songs_in_the_library(self):
+        music_library = MusicLibrary()
+        music_library.add("Rolling Blackouts by The Go! Team")
+        music_library.add("Oh Yeah by Locust")
+        self.assertEqual(music_library.all(), ["Rolling Blackouts by The Go! Team", "Oh Yeah by Locust"])
+
+    def test_removes_songs_from_library(self):
+        music_library = MusicLibrary()
+        music_library.add("Rolling Blackouts by The Go! Team")
+        music_library.add("Oh Yeah by Locust")
+        music_library.remove(1)
+        self.assertEqual(music_library.trackList, ["Rolling Blackouts by The Go! Team"])
+
+    def test_returns_false_if_removing_song_not_in_list(self):
+        music_library = MusicLibrary()
+        self.assertFalse(music_library.remove(20))
