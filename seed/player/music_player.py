e82e146f94205da36e41a2dd0e8c82cbb3ba456c
benfielding74 <billy.fadden@live.com>
2022-02-25 14:44:37 +0000

    First unit tests written for music player driving build of player, need to examine Mocks behaviour

--- seed/player/music_player.py
+++ seed/player/music_player.py
@@ -1,4 +1,12 @@
 import subprocess
 
 class MusicPlayer:
-    pass
+
+    def __init__(self, args):
+        self.args = args
+
+    def play(self, track):# will get passed a track file and will play it
+        self.track = track # which will be an mp3 file name
+        return self.args.call(["afplay", f"{self.track}"])
+
+# The interface is doing the heavy lifting and interaction to check if track exists
--- seed/tests/test_music_player.py
+++ seed/tests/test_music_player.py
@@ -1,10 +1,13 @@
 import unittest
+import subprocess
 from unittest.mock import Mock
 from player.music_player import MusicPlayer
 
 class TestMusicPlayer(unittest.TestCase):
     def test_constructs(self):
-        MusicPlayer()
+        MusicPlayer(subprocess)
 
     def test_plays_a_track(self):
-        
+        mock_track = Mock('data/tunes/myfav.wav') # should I also mock the player?
+        music_player = MusicPlayer(subprocess)
+        self.assertEqual(music_player.play(mock_track), 0)
