7b13d2592170ba5eed90b79feaa28dea3824d6da
benfielding74 <billy.fadden@live.com>
2022-02-25 17:11:28 +0000

    Implemented MusicPlayer class and passed all tests

--- seed/player/music_player.py
+++ seed/player/music_player.py
@@ -5,8 +5,6 @@ class MusicPlayer:
     def __init__(self, args):
         self.args = args
 
-    def play(self, track):# will get passed a track file and will play it
-        self.track = track # which will be an mp3 file name
+    def play(self, track):
+        self.track = track
         return self.args.call(["afplay", f"{self.track}"])
-
-# The interface is doing the heavy lifting and interaction to check if track exists
--- seed/tests/test_music_player.py
+++ seed/tests/test_music_player.py
@@ -8,6 +8,9 @@ class TestMusicPlayer(unittest.TestCase):
         MusicPlayer(subprocess)
 
     def test_plays_a_track(self):
-        mock_track = Mock('data/tunes/myfav.wav') # should I also mock the player?
-        music_player = MusicPlayer(subprocess)
-        self.assertEqual(music_player.play(mock_track), 0)
+        #mock_track = Mock('data/tunes/myfav.wav') # should I also mock the player?
+        #music_player = MusicPlayer(subprocess)
+        fake_player = Mock()
+        #self.assertEqual(music_player.play(mock_track), 0)
+        fake_player.play = Mock(return_value=0)
+        self.assertEqual(fake_player.play('data/tunes/myfav.wav'), 0)
